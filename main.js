6942bd33bea04bdfad730d7e9bd039b4a2e5fa7f
Ken Dale <ken@kendaleiv.com>
2014-01-29 16:20:47 -0500

    Use done rather than jQuery.ajax success

--- main.js
+++ main.js
@@ -6,14 +6,13 @@
             $.ajax('http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol in ("' + stockSymbol + '")&diagnostics=true&env=http://datatables.org/alltables.env', {
                 beforeSend: function () {
                     $('#current-stock-price').html('Loading ...');
-                },
-                success: function (res) {
-                    var quote = $(res).find('[symbol="' + stockSymbol + '"]');
-                    var lastTradePrice = quote.find('LastTradePriceOnly').text();
-
-                    $('#current-stock-price').html('<strong>' + stockSymbol + '</strong>: $' + lastTradePrice + ' retrieved at ' + new Date().toString());
-                    $('#stock-price-log').html($('#stock-price-log').html() + '<li><strong>' + stockSymbol + '</strong> $' + lastTradePrice + ' retrieved at ' + new Date().toString() + '</li>');
                 }
+            }).done(function (res) {
+                var quote = $(res).find('[symbol="' + stockSymbol + '"]');
+                var lastTradePrice = quote.find('LastTradePriceOnly').text();
+
+                $('#current-stock-price').html('<strong>' + stockSymbol + '</strong>: $' + lastTradePrice + ' retrieved at ' + new Date().toString());
+                $('#stock-price-log').html($('#stock-price-log').html() + '<li><strong>' + stockSymbol + '</strong> $' + lastTradePrice + ' retrieved at ' + new Date().toString() + '</li>');
             });
         });
     });
