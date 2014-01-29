66a2c5f0e5e77b9f4387b1c9478dc63f1bd3eac3
Ken Dale <ken@kendaleiv.com>
2014-01-29 16:47:31 -0500

    Remove jQuery.ajax beforeSend, extract url var

--- main.js
+++ main.js
@@ -7,11 +7,10 @@
         $('#fetch').on('click', function () {
             var stockSymbol = $.trim($stockSymbol.val().toUpperCase());
 
-            $.ajax('http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol in ("' + stockSymbol + '")&diagnostics=true&env=http://datatables.org/alltables.env', {
-                beforeSend: function () {
-                    $currentStockPrice.html('Loading ...');
-                }
-            }).done(function (res) {
+            $currentStockPrice.html('Loading ...');
+
+            var url = 'http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol in ("' + stockSymbol + '")&diagnostics=true&env=http://datatables.org/alltables.env';
+            $.ajax(url).done(function (res) {
                 var quote = $(res).find('[symbol="' + stockSymbol + '"]');
                 var lastTradePrice = quote.find('LastTradePriceOnly').text();
                 var dateString = new Date().toString();
