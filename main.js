58a187eccd16e3b6af445cc1b9a8844f6553a3c8
Ken Dale <ken@kendaleiv.com>
2014-01-29 16:24:22 -0500

    Cache repeatedly used jQuery selectors

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 (function ($) {
     $(function () {
+        var $stockSymbol = $('#stock-symbol');
+        var $currentStockPrice = $('#current-stock-price');
+        var $stockPriceLog = $('#stock-price-log');
+
         $('#fetch').on('click', function () {
-            var stockSymbol = $.trim($('#stock-symbol').val().toUpperCase());
+            var stockSymbol = $.trim($stockSymbol.val().toUpperCase());
 
             $.ajax('http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol in ("' + stockSymbol + '")&diagnostics=true&env=http://datatables.org/alltables.env', {
                 beforeSend: function () {
-                    $('#current-stock-price').html('Loading ...');
+                    $currentStockPrice.html('Loading ...');
                 }
             }).done(function (res) {
                 var quote = $(res).find('[symbol="' + stockSymbol + '"]');
                 var lastTradePrice = quote.find('LastTradePriceOnly').text();
 
-                $('#current-stock-price').html('<strong>' + stockSymbol + '</strong>: $' + lastTradePrice + ' retrieved at ' + new Date().toString());
-                $('#stock-price-log').html($('#stock-price-log').html() + '<li><strong>' + stockSymbol + '</strong> $' + lastTradePrice + ' retrieved at ' + new Date().toString() + '</li>');
+                $currentStockPrice.html('<strong>' + stockSymbol + '</strong>: $' + lastTradePrice + ' retrieved at ' + new Date().toString());
+                $stockPriceLog.html($stockPriceLog.html() + '<li><strong>' + stockSymbol + '</strong> $' + lastTradePrice + ' retrieved at ' + new Date().toString() + '</li>');
             });
         });
     });
