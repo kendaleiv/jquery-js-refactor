32463dd464f207252de2f365e23f4c768da22db4
Ken Dale <ken@kendaleiv.com>
2016-08-20 09:50:00 -0400

    Enable linter on src/index.js, fix issues

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-/* eslint-disable */
+/* eslint-env jquery */
 
 $(document).ready(function () {
-    $('#fetch').click(function () {
-        var stockSymbol = $.trim($('#stock-symbol').val().toUpperCase());
+  $('#fetch').click(function () {
+    const stockSymbol = $.trim($('#stock-symbol').val().toUpperCase());
+    const url = 'http://query.yahooapis.com/v1/public/yql'
+      + `?q=select * from yahoo.finance.quotes where symbol in ("${stockSymbol}")`
+      + '&diagnostics=true'
+      + '&env=http://datatables.org/alltables.env';
 
-        $.ajax('http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol in ("' + stockSymbol + '")&diagnostics=true&env=http://datatables.org/alltables.env', {
-            beforeSend: function() {
-                $('#current-stock-price').html('Loading ...');
-            },
-            success: function (res) {
-                var quote = $(res).find('[symbol="' + stockSymbol + '"]');
-                var lastTradePrice = quote.find('LastTradePriceOnly').text();
+    $.ajax(url, {
+      beforeSend: function () {
+        $('#current-stock-price').html('Loading ...');
+      },
+      success: function (res) {
+        const quote = $(res).find(`[symbol="${stockSymbol}"]`);
+        const lastTradePrice = quote.find('LastTradePriceOnly').text();
 
-                $('#current-stock-price').html('<strong>' + stockSymbol + '</strong>: $' + lastTradePrice + ' retrieved at ' + new Date().toString());
-                $('#stock-price-log').html($('#stock-price-log').html() + '<li><strong>' + stockSymbol + '</strong> $' + lastTradePrice + ' retrieved at ' + new Date().toString() + '</li>');
-            }
-        });
+        $('#current-stock-price').html(
+          `<strong>${stockSymbol}</strong>: $${lastTradePrice}`
+          + ` retrieved at ${new Date().toString()}`);
+
+        $('#stock-price-log').html(
+          `${$('#stock-price-log').html()}<li><strong>${stockSymbol}</strong> $${lastTradePrice}`
+          + ` retrieved at ${new Date().toString()}</li>`);
+      }
     });
+  });
 });
