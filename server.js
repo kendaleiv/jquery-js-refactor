cd4b9f1f023987a37c4f851756863ff0994427c3
Ken Dale <ken@kendaleiv.com>
2016-08-20 09:11:59 -0400

    Move inline JS out of index.html into webpack bundle

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
 const port = process.env.port || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile('./index.html', { root: path.join(__dirname, 'public') });
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
